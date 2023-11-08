9b29bc89eeec10a399f458d3bf0595be4d2ae2d6
Paulo Hernane <paulohernane10@gmail.com>
2023-11-08 12:14:33 -0300

    Remove unnecessary tokens from .env files and env.ts

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const envSchema = zod.object({
   PORT: zod.string().min(1).default('8080').readonly(),
   JWT_SECRET: zod.string().min(1).default('secret').readonly(),
+  ACCESS_TOKEN_SECRET: zod.string().min(1).default('secret').readonly(),
+  REFRESH_TOKEN_SECRET: zod.string().min(1).default('secret').readonly(),
 });
 
 export const env = envSchema.parse(process.env);
