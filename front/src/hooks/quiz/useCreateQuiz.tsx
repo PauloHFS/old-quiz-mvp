526329484aa2ae4b8771945b2a46ccbf5ca1e8bc
Paulo Hernane <paulohernane10@gmail.com>
2023-11-10 23:15:26 -0300

    Add validation to quizSchema in useCreateQuiz hook

--- front/src/hooks/quiz/useCreateQuiz.tsx
+++ front/src/hooks/quiz/useCreateQuiz.tsx
@@ -3,14 +3,26 @@ import * as z from 'zod';
 import { apiClient } from '../../services/api';
 
 const quizSchema = z.object({
-  nome: z.string(),
-  questoes: z.array(
-    z.object({
-      titulo: z.string(),
-      alternativas: z.array(z.string()),
-      correctIndex: z.number(),
-    })
-  ),
+  nome: z
+    .string()
+    .min(2, 'nome precisa ter no mínimo 2 caracteres')
+    .max(100, 'nome precisa ter no máximo 100 caracteres'),
+  questoes: z
+    .array(
+      z.object({
+        titulo: z
+          .string()
+          .min(2, 'titulo precisa ter no mínimo 2 caracteres')
+          .max(100, 'titulo precisa ter no máximo 100 caracteres'),
+        alternativas: z
+          .array(z.string())
+          .min(2, 'precisa ter no mínimo 2 alternativas')
+          .max(4, 'precisa ter no máximo 4 alternativas'),
+        correctIndex: z.number(),
+      })
+    )
+    .min(1, 'precisa ter no mínimo 1 questão')
+    .max(10, 'precisa ter no máximo 10 questões'),
 });
 
 type Quiz = z.infer<typeof quizSchema>;
