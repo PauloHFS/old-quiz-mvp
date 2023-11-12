e93335cbf66c8900914da183af433a0847e09bc6
Paulo Hernane <paulohernane10@gmail.com>
2023-11-12 15:42:28 -0300

    Refactor newQuiz form validation and add error messages

--- front/src/pages/v1/newQuiz.tsx
+++ front/src/pages/v1/newQuiz.tsx
@@ -1,21 +1,24 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Button } from '../../components/Button';
 
 const schema = z.object({
   name: z.string().min(1),
-  questions: z.array(
-    z.object({
-      title: z.string().min(1),
-      alternatives: z.array(z.string().min(1)),
-      correctID: z.number().int().min(0),
-    })
-  ),
+  questions: z
+    .array(
+      z.object({
+        title: z.string().min(1, 'A questão está vazia!'),
+        alternatives: z
+          .array(z.string().min(1, 'A alternativa está vazia!'))
+          .min(2)
+          .max(4),
+        correctID: z.number().int(),
+      })
+    )
+    .min(1, 'Adicione pelo menos uma questão!')
+    .max(10, 'Limite de 10 questões atingido!'),
 });
-
-const regexIndex = /\[(\d+)\]/;
 
 type NewQuizFormData = z.infer<typeof schema>;
 
@@ -23,9 +26,16 @@ export const NewQuiz = () => {
   const {
     register,
     handleSubmit,
+    setValue,
+    watch,
+    trigger,
     formState: { errors },
   } = useForm<NewQuizFormData>({
     resolver: zodResolver(schema),
+    defaultValues: {
+      name: '',
+      questions: [],
+    },
   });
 
   const onSubmit = (data: NewQuizFormData) => {
@@ -38,10 +48,21 @@ export const NewQuiz = () => {
     correctID: 0,
   };
 
-  const [questions, setQuestions] = useState<NewQuizFormData['questions']>([]);
+  const questions = watch('questions');
+  type SetQuestions = (
+    p: NewQuizFormData['questions']
+  ) => NewQuizFormData['questions'];
+  const setQuestions = (args: NewQuizFormData['questions'] | SetQuestions) => {
+    if (typeof args === 'function') {
+      setValue('questions', args(questions));
+      return;
+    }
+    setValue('questions', args);
+  };
 
   const addQuestion = () => {
     setQuestions(prev => [...prev, defaultQuestion]);
+    // trigger('questions');
   };
 
   const removeQuestion = (questionIndex: number) => {
@@ -49,6 +70,7 @@ export const NewQuiz = () => {
       ...prev.slice(0, questionIndex),
       ...prev.slice(questionIndex + 1),
     ]);
+    trigger('questions');
   };
 
   const addAlternative = (questionIndex: number) => {
@@ -64,6 +86,7 @@ export const NewQuiz = () => {
         ...prev.slice(questionIndex + 1),
       ];
     });
+    trigger(`questions.${questionIndex}.alternatives`);
   };
 
   const removeAlternative = (
@@ -85,21 +108,19 @@ export const NewQuiz = () => {
         ...prev.slice(questionIndex + 1),
       ];
     });
+    trigger(`questions.${questionIndex}.alternatives`);
   };
 
   const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
 
     const nameParts = name.split('.');
-    const indexMatch = nameParts[0].match(regexIndex)?.[1];
-    if (indexMatch === undefined) {
-      return;
-    }
+    console.log(nameParts);
 
-    const questionIndex = Number(indexMatch);
+    const questionIndex = Number(nameParts[1]);
     const question = questions[questionIndex];
 
-    if (nameParts[1] === 'title') {
+    if (nameParts[2] === 'title') {
       const newQuestion = {
         ...question,
         title: value,
@@ -112,16 +133,12 @@ export const NewQuiz = () => {
       ];
 
       setQuestions(newQuestions);
+      trigger(`questions.${questionIndex}.title`);
       return;
-    } else if (nameParts[1].includes('alternatives')) {
-      const indexMatch = nameParts[1].match(regexIndex)?.[1];
-      if (indexMatch === undefined) {
-        return;
-      }
+    } else if (nameParts[2] === 'alternatives') {
+      const alternativeIndex = Number(nameParts[3]);
 
-      const alternativeIndex = Number(indexMatch);
-
-      if (nameParts[2] === 'title') {
+      if (nameParts?.[4] === undefined) {
         const newQuestion = {
           ...question,
           alternatives: [
@@ -138,8 +155,9 @@ export const NewQuiz = () => {
         ];
 
         setQuestions(newQuestions);
+        trigger(`questions.${questionIndex}.alternatives`);
         return;
-      } else if (nameParts[2] === 'correctID') {
+      } else if (nameParts[4] === 'correctID') {
         const newQuestion = {
           ...question,
           correctID: alternativeIndex,
@@ -152,6 +170,7 @@ export const NewQuiz = () => {
         ];
 
         setQuestions(newQuestions);
+        trigger(`questions.${questionIndex}.alternatives`);
         return;
       }
     }
@@ -171,22 +190,32 @@ export const NewQuiz = () => {
         </label>
         <div>
           <h2>2. Adicione questões da sua pesquisa:</h2>
+          {errors.questions && <span>{errors.questions.message}</span>}
         </div>
         {questions.map((question, questionIndex) => (
           <div key={questionIndex}>
             <div className="flex gap-2">
               <h3>{`${questionIndex + 1}. `}</h3>
-              <input
-                type="text"
-                name={`questions[${questionIndex}].title`}
-                placeholder="Você gostou do evento?"
-                className="border-b-2 border-gray-300"
-                value={question.title}
-                onChange={handleFormChange}
-              />
-              <Button.Primary onClick={() => removeQuestion(questionIndex)}>
-                -
-              </Button.Primary>
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  name={`questions.${questionIndex}.title`}
+                  placeholder="Você gostou do evento?"
+                  className="border-b-2 border-gray-300"
+                  value={question.title}
+                  onChange={handleFormChange}
+                />
+                {errors.questions?.[questionIndex]?.title && (
+                  <span>
+                    {errors.questions?.[questionIndex]?.title?.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <Button.Primary onClick={() => removeQuestion(questionIndex)}>
+                  -
+                </Button.Primary>
+              </div>
             </div>
             <div>
               {question.alternatives.map((alternative, alternativeIndex) => (
@@ -197,27 +226,42 @@ export const NewQuiz = () => {
                   <h4>
                     {questionIndex + 1}.{alternativeIndex + 1}.{' '}
                   </h4>
-                  <input
-                    type="text"
-                    placeholder={`Sim, não, talvez...`}
-                    className="rounded-md border-b-2 border-gray-300"
-                    name={`questions[${questionIndex}].alternatives[${alternativeIndex}].title`}
-                    value={alternative}
-                    onChange={handleFormChange}
-                  />
+                  <div className="flex flex-col">
+                    <input
+                      type="text"
+                      placeholder={`Sim, não, talvez...`}
+                      className="rounded-md border-b-2 border-gray-300"
+                      name={`questions.${questionIndex}.alternatives.${alternativeIndex}`}
+                      value={alternative}
+                      onChange={handleFormChange}
+                    />
+                    {errors.questions?.[questionIndex]?.alternatives?.[
+                      alternativeIndex
+                    ] && (
+                      <span>
+                        {
+                          errors.questions?.[questionIndex]?.alternatives?.[
+                            alternativeIndex
+                          ]?.message
+                        }
+                      </span>
+                    )}
+                  </div>
                   <input
                     type="radio"
-                    name={`questions[${questionIndex}].alternatives[${alternativeIndex}].correctID`}
+                    name={`questions.${questionIndex}.alternatives.${alternativeIndex}.correctID`}
                     checked={alternativeIndex === question.correctID}
                     onChange={handleFormChange}
                   />
-                  <Button.Primary
-                    onClick={() =>
-                      removeAlternative(questionIndex, alternativeIndex)
-                    }
-                  >
-                    -
-                  </Button.Primary>
+                  <div>
+                    <Button.Primary
+                      onClick={() =>
+                        removeAlternative(questionIndex, alternativeIndex)
+                      }
+                    >
+                      -
+                    </Button.Primary>
+                  </div>
                 </div>
               ))}
             </div>
