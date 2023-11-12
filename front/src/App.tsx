a8259500584790fbb0c0a31f7ec19f22bc290fac
Paulo Hernane <paulohernane10@gmail.com>
2023-11-12 16:22:21 -0300

    Add import for ReactToastify CSS.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { router } from './pages';
 
 const queryClient = new QueryClient();
