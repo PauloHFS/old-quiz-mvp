c1aa5512b69d9ccc05086d8fbc5c76dbb0980adc
Paulo Hernane <paulohernane10@gmail.com>
2023-12-01 00:19:51 -0300

    Refactor quiz stats calculation

--- server/src/modules/quiz/controllers.ts
+++ server/src/modules/quiz/controllers.ts
@@ -145,16 +145,6 @@ export const getQuizStats = async (req: Request, res: Response) => {
     // TODO Add validation
     const quizId = Number(req.params.id);
 
-    /*
-    Stats:
-    - total responses
-    - right answers
-    - wrong answers
-
-    - age
-    - gender
-    */
-
     const quiz = await prismaClient.quiz.findUnique({
       where: {
         id: quizId,
@@ -177,43 +167,83 @@ export const getQuizStats = async (req: Request, res: Response) => {
       },
     });
 
-    // Map<questionId, Map<alternative, count>>
-    // const questionAnswersCountMap = new Map<number, Map<string, number>>();
+    // - total de respostas por quiz
+    const total = responseUserInfos.length;
 
+    // - total dividido por genero
+    const genderCountMap = {
+      female: 0,
+      male: 0,
+    };
+
+    responseUserInfos.forEach(({ gender }) => {
+      if (!(gender in genderCountMap)) return;
+      const k = gender as 'female' | 'male';
+      genderCountMap[k] += 1;
+    });
+
+    // - total dividido por idade
+    const ageCountMap: { [age: number]: number } = {};
+
+    responseUserInfos.forEach(({ age }) => {
+      const count = ageCountMap[age] || 0;
+      ageCountMap[age] = count + 1;
+    });
+
+    // - ranking de top 3 cidades com mais respostas
+    const geolocationCountMap: { [geolocation: string]: number } = {};
+    responseUserInfos.forEach(({ geolocation }) => {
+      const count = geolocationCountMap[geolocation] || 0;
+      geolocationCountMap[geolocation] = count + 1;
+    });
+    const top3Geolocation = Object.entries(geolocationCountMap)
+      .sort(([, countA], [, countB]) => countB - countA)
+      .slice(0, 3)
+      .map(([geolocation]) => geolocation);
+
+    // - total de respostas por pergunta
     const questionIdToTitle = new Map<number, string>();
     quiz.Questions.forEach(({ id, title }) => {
       questionIdToTitle.set(id, title);
     });
 
-    const questionAnswersCountMap: {
-      [questionTitle: string]: {
-        [alternative: string]: number;
-      };
-    } = {};
+    const questionStats: {
+      questionTitle: string;
+      alternatives: {
+        alternative: string;
+        count: number;
+        correct: boolean;
+      }[];
+    }[] = [];
 
-    responseUserInfos.forEach(({ Responses }) => {
-      Responses.forEach(({ alternative, questionId }) => {
-        const questionTitle = questionIdToTitle.get(questionId);
-        if (!questionTitle) return;
-        const questionMap = questionAnswersCountMap[questionTitle];
+    quiz.Questions.forEach(({ id, alternatives, correctIndex }) => {
+      const alternativeCountMap: { [alternative: string]: number } = {};
+      responseUserInfos.forEach(({ Responses }) => {
+        const response = Responses.find(({ questionId }) => questionId === id);
+        if (!response) return;
+        const { alternative } = response;
+        const count = alternativeCountMap[alternative] || 0;
+        alternativeCountMap[alternative] = count + 1;
+      });
 
-        if (questionMap) {
-          const count =
-            questionAnswersCountMap[questionTitle][alternative] || 0;
-          questionAnswersCountMap[questionTitle][alternative] = count + 1;
-        } else {
-          questionAnswersCountMap[questionTitle] = {
-            [alternative]: 1,
-          };
-        }
+      const alternativesStats = alternatives.map((alternative, index) => ({
+        alternative,
+        count: alternativeCountMap[alternative] || 0,
+        correct: index === correctIndex,
+      }));
+
+      questionStats.push({
+        questionTitle: questionIdToTitle.get(id) || '',
+        alternatives: alternativesStats,
       });
     });
 
     return res.json({
-      quiz,
-      questionAnswersCountMap,
-      responseUserInfo: responseUserInfos[0],
-      totalAnswers: responseUserInfos.length,
+      total,
+      genderTotal: genderCountMap,
+      ageTotal: ageCountMap,
+      geoTotal: top3Geolocation,
+      questionTotal: questionStats,
     });
   } catch (error) {
     return res.status(400).json(error);
